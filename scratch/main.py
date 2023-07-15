afb1d9df8106a8007959d2b061d4f1f35e778990
Zili Wang <283136745@qq.com>
2023-07-15 15:52:43 +0800

    asdfds

--- scratch/main.py
+++ scratch/main.py
@@ -5,9 +5,15 @@ from PIL import Image, ImageDraw
 
 f = open("mnist_test.csv","r")
 line_text = f.readline()
-print(len(line_text))
-# img = Image.new('RGB', (32, 32), color = 'black')
-# draw = ImageDraw.Draw(img)
-# img.putpixel((10,20),(255,0,0,))
-# img.save('output.bmp')
+line_text = f.readline()
+a_vec = line_text.split(",")
+label=int(a_vec[0])
+img_s=28
+img = Image.new('RGB', (img_s, img_s), color = 'black')
+draw = ImageDraw.Draw(img)
+for i in range(img_s):
+    for j in range(img_s):
+        gray=int(a_vec[i*img_s+j+1])
+        img.putpixel((j,i),(gray,gray,gray))
+img.save('output.bmp')
 
