487b25bf8b55a40e0f6352e96c2f22c8b5c40564
Zili Wang <283136745@qq.com>
2023-07-24 18:59:13 +0800

    sssdf

--- scratch/main.py
+++ scratch/main.py
@@ -19,62 +19,98 @@ def show_img():
     img.save('output.bmp')
     img.show()
 
+def softmax(z):
+    z = z - np.max(z)
+    return np.exp(z) / np.sum(np.exp(z))
 
-# mlp only in numpy of tree layers
+def ReLU(x):
+    return np.maximum(0,x)
+
+def dReLU(x):
+    return 1 * (x > 0) 
+
 class MLP:
-    def __init__(self, input_size, hidden_size, output_size):
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+    def __init__(self):
+        self.input_size = 784
+        self.hidden_size = [256,128]
+        self.output_size = 10
 
-        self.W1 = np.random.randn(input_size, hidden_size)
-        self.b1 = np.random.randn(hidden_size)
-        self.W2 = np.random.randn(hidden_size, output_size)
-        self.b2 = np.random.randn(output_size)
+        self.W1 = np.random.randn(self.hidden_size[0], self.input_size)
+        self.b1 = np.random.randn(self.hidden_size[0],1)
+        self.W2 = np.random.randn(self.hidden_size[1], self.hidden_size[0])
+        self.b2 = np.random.randn(self.hidden_size[1],1)
+        self.W3 = np.random.randn(self.output_size, self.hidden_size[1])
+        self.b3 = np.random.randn(self.output_size,1)
+        self.lr = 0.001
+
 
     def forward(self, x):
         self.x = x
-        self.z1 = np.dot(x, self.W1) + self.b1
-        self.h1 = np.maximum(0, self.z1)  # ReLU
-        self.z2 = np.dot(self.h1, self.W2) + self.b2
-        self.h2 = np.exp(self.z2) / np.sum(np.exp(self.z2))  # softmax
-        result = np.argmax(self.h2, axis=0)
+        self.z1 = np.matmul(self.W1, x) + self.b1 # (256, 784) * (784, n) + (256, 1) = (256, n)
+        self.a1 = ReLU(self.z1)
+        self.z2 = np.matmul(self.W2, self.a1) + self.b2 # (128, 256) * (256, n) + (128, 1) = (128, n)
+        self.a2 = ReLU(self.z2)
+        self.z3 = np.matmul(self.W3, self.a2) + self.b3 # (10, 128) * (128, n) + (10, 1) = (10, n)
+        self.a3 = softmax(self.z3)
+        result = np.argmax(self.a3, axis=0)
         return result
 
-    def loss_entropy(self, y):
-        y = np.eye(self.output_size)[y]
-        return -np.sum(y * np.log(self.h2 + 1e-7)) / len(y)
-
     def backward(self, y):
-        y = np.eye(self.output_size)[y]
-        delta3 = self.h2 - y
-        self.dW2 = np.dot(self.h1.T, delta3)
-        self.db2 = np.sum(delta3, axis=0)
-        delta2 = np.dot(delta3, self.W2.T) * (self.z1 > 0)
-        self.dW1 = np.dot(self.x.T, delta2)
-        self.db1 = np.sum(delta2, axis=0)
+        delta3 = (1/y.shape[1])*(self.a3 - y)
+        loss=np.mean((self.a3 - y)**2)
+        DW3 = np.matmul(delta3,self.a2.T) # (10, n) * (n, 128) = (10, 128)
+        Da2 = np.matmul(delta3.T, self.W3).T # (n, 10) * (10, 128) = (128, n)
+        Dz2 = Da2 * dReLU(self.z2) 
+        DW2 = np.matmul(Dz2,self.a1.T) # (128, n) * (n, 256) = (128, 256)
+        Da1 = np.matmul(Dz2.T, self.W2).T # (n, 128) * (128, 256) = (256, n)
+        Dz1 = Da1 * dReLU(self.z1)
+        DW1 = np.matmul(Dz1, self.x.T) # (256, n) * (n, 784) = (256, 784)
 
-
-    def update(self, lr=0.01):
-        self.W1 -= lr * self.dW1
-        self.b1 -= lr * self.db1
-        self.W2 -= lr * self.dW2
-        self.b2 -= lr * self.db2
+        Db3 = np.sum(delta3,axis=1,keepdims=True)
+        Db2 = np.sum(Dz2,axis=1,keepdims=True)
+        Db1 = np.sum(Dz1,axis=1,keepdims=True)
+        #update weights
+        self.W3 -= self.lr * DW3
+        self.W2 -= self.lr * DW2
+        self.W1 -= self.lr * DW1
+        self.b3 -= self.lr * Db3
+        self.b2 -= self.lr * Db2
+        self.b1 -= self.lr * Db1
+        #print(np.mean(DW3))
 
 
 def read_a_line(f):
     line_text = f.readline()
+    if len(line_text)==0:
+        return [], []
     a_vec = line_text.split(",")
-    label = int(a_vec[0])
+    label = np.zeros(10)
+    label[int(a_vec[0])] = 1 
     img_data=[]
     for i in range(1,len(a_vec)):
         img_data.append(float(a_vec[i])/255.0)
     return label, img_data
 
+
 f = open("mnist_test.csv","r")
-label, img_data = read_a_line(f)
-mlp = MLP(img_s*img_s, 100, 10)
-img_data = np.array(img_data)
-output = mlp.forward(img_data)
-print(output)
+labels=[]
+img_datas=[]
+while True:
+    label, img_data = read_a_line(f)
+    if len(label)==0:
+        break
+    labels.append(label)
+    img_datas.append(img_data)
+
+#get a random batch
+batch_size=64
+mlp = MLP()
+for i in range(100):
+    idx = np.random.randint(0, len(labels), batch_size)
+    label = np.array(labels)[idx]
+    label=label.T
+    img_data = np.array(img_datas)[idx]
+    img_data=img_data.T
+    output = mlp.forward(img_data)
+    mlp.backward(label)
 
---
+++ scratch/test_numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#get matrix 2*3
+a = np.array([[1,2,3],[4,5,6]])
+#get matrix 3*1
+b = np.array([[1,2,3]])
+#matrix product
+print(a+b)
